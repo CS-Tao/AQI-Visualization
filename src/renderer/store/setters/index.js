d467ef8a52a624150f9cd608bba4bd43067300a9
CS-Tao <990296951@qq.com>
2018-09-18 11:29:09 +0800

    添加全局变量

--- src/renderer/store/setters/index.js
+++ src/renderer/store/setters/index.js
@@ -1,6 +1,8 @@
 const getters = {
-  mapCenterLat: state => state.mapConfig.centerLat,
-  mapCenterLng: state => state.mapConfig.centerLng
+  mapCenterLat: state => state.app.mapConfig.centerLat,
+  mapCenterLng: state => state.app.mapConfig.centerLng,
+  selectedDateStr: state => state.app.mapConfig.selectedDateStr,
+  maskMainView: state => state.app.maskMainView
 }
 
 export default getters
