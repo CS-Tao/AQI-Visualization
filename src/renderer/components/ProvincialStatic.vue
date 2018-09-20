0a8688796ffad02231b4ad2ca1c8e5af42a57ecf
Unknown <1454620282@qq.com>
2018-09-20 17:03:06 +0800

    添加城市树

--- src/renderer/components/ProvincialStatic.vue
+++ src/renderer/components/ProvincialStatic.vue
@@ -2,26 +2,39 @@
   <!-- <json-map class="container" :json-data="jsonData"/> -->
   <div >
     <el-select class="select" v-model="selectId" filterable @change="getProvinceData()" placeholder="请选择">
-    <el-option
-      v-for="item in provinceNames"
-      :key="item.id"
-      :label="item.name"
-      :value="item.id">
-    </el-option>
-  </el-select>
-    <div v-if="show">
+      <el-option
+        v-for="item in provinceNames"
+        :key="item.id"
+        :label="item.name"
+        :value="item.id">
+      </el-option>
+    </el-select>
+    <div v-if="treeData.length!==0" class="primary-panel tree">
+      <el-tree
+      ref="tree"
+      :data="treeData" 
+      show-checkbox
+      node-key="id"
+      :default-expanded-keys="[0]"
+      @check-change="treeNodeCheck"
+      :props="{
+          children: 'children',
+          label: 'label'
+        }">
+      </el-tree>
+    </div>
+    <div v-if="showData.length!==0">
       <div class="dashbord">
         <dashbord v-bind:num="message"></dashbord>
       </div>
       <div class="lines">
-        <lines style="left:0;bottom:0;" v-on:sendavery="getavery" :xaxis="ProvinceData.xaxis" :sseries="ProvinceData.datalist"> </lines>
+        <lines style="left:0;bottom:0;" v-on:sendavery="getavery" :xaxis="ProvinceData.xaxis" :sseries="showData"> </lines>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import demoApi from '@/api/demo.api'
 import provinceApi from '@/api/province.api'
 import Map from '@/components/ProvincialStatic/JSONMap'
 import dashbord from '@/components/ProvincialStatic/Dashbord'
@@ -34,8 +47,8 @@ export default {
       selectId: '',
       provinceNames: [],
       ProvinceData: {},
-      message: [0, '2017'],
-      show: false
+      showData: [],
+      message: [0, '2017']
     }
   },
   components: {
@@ -43,15 +56,35 @@ export default {
     dashbord,
     lines
   },
+  computed: {
+    selectName () {
+      return this.provinceNames.filter((item) => {
+        return item.id === this.selectId
+      })[0].name
+    },
+    treeData () {
+      if (this.ProvinceData.datalist) {
+        var childrenNodes = []
+        for (let i = 0; i < this.ProvinceData.datalist.length; i++) {
+          childrenNodes.push({
+            id: i + 1,
+            label: this.ProvinceData.datalist[i].name
+          })
+        }
+        return [{
+          id: 0,
+          label: this.selectName,
+          children: childrenNodes
+        }]
+      } else {
+        return []
+      }
+    }
+  },
   mounted () {
     this.getProvinceName()
   },
   methods: {
-    getjsonData () {
-      demoApi.getRequest().then((response) => {
-        this.jsonData = response.data
-      }).catch(() => {})
-    },
     getProvinceName () {
       provinceApi.getProvinceName().then((response) => {
         if (response.status === 200) {
@@ -64,17 +97,23 @@ export default {
         provinceApi.getProvinceData(this.selectId).then((response) => {
           if (response.status === 200) {
             this.ProvinceData = response.data
-            // console.log('response.data', JSON.stringify(response.data))
           }
-          this.show = true
-          // this.$triggerResize()
         }).catch(() => {})
-      } else {
-        this.show = false
       }
     },
     getavery: function (msg) {
       this.message = msg
+    },
+    treeNodeCheck () {
+      var checkedNodes = this.$refs.tree.getCheckedNodes()
+      this.showData = this.ProvinceData.datalist.filter((item) => {
+        for (let i = 0; i < checkedNodes.length; i++) {
+          if (item.name === checkedNodes[i].label) {
+            return true
+          }
+        }
+        return false
+      })
     }
   }
 }
@@ -102,6 +141,14 @@ export default {
   right: 15px;
   width: 240px;
   height: 32px;
+}
+.tree {
+  position: fixed;
+  overflow-y: scroll;
+  top: calc(100/1920*111vw + 62px);
   right: 15px;
+  width: 240px;
+  height: 40vh; 
+  padding: 0;
 }
 </style>
